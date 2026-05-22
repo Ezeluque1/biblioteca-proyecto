1a2282ea5065564e464e9c6764bc8e5c60e4f0a0
Esteban Frassa <esteban.frassa@gmail.com>
2026-05-22 19:52:11 -0300

    Implementado singleton y busqueda de libros

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -1,12 +1,14 @@
 package com.biblioteca;
-import  com.biblioteca.model.Biblioteca;
+
+import com.biblioteca.model.Biblioteca;
 import com.biblioteca.model.Libro;
+import com.biblioteca.singleton.BibliotecaSingleton;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Biblioteca biblio = new Biblioteca();
+        Biblioteca biblio = BibliotecaSingleton.getInstancia();
 
         biblio.agregarLibro(new Libro("Harry Potter"));
         biblio.agregarLibro(new Libro("El Señor de los Anillos"));
