a0838492acc2117163e332b958e836125a51e9db
Esteban Frassa <esteban.frassa@gmail.com>
2026-05-22 19:58:33 -0300

    Agregada demostracion de busqueda en Main

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -13,8 +13,20 @@ public class Main {
         biblio.agregarLibro(new Libro("Harry Potter"));
         biblio.agregarLibro(new Libro("El Señor de los Anillos"));
 
+        System.out.println("Lista de libros:");
+
         for (Libro l : biblio.getLibros()) {
             System.out.println(l.getTitulo());
+        }
+
+        System.out.println();
+
+        Libro encontrado = biblio.buscarLibro("Harry Potter");
+
+        if (encontrado != null) {
+            System.out.println("Libro encontrado: " + encontrado.getTitulo());
+        } else {
+            System.out.println("Libro no encontrado");
         }
     }
 }
